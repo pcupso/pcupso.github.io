e7f46d59d04656f6b5d5b16c857cd9d6edd2cccc
Cai Chao <893710128@qq.com>
2019-12-13 22:39:37 +0800

    更新网站

---
+++ precache-manifest.ba660eadd3af05c7fa98f8cdd7c451b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "faa349da7f4dba26b7a07f2053e4833d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "90a60445b057c314189b",
+    "url": "/static/css/main.6ff62aac.chunk.css"
+  },
+  {
+    "revision": "43e67d421d837da4710f",
+    "url": "/static/js/2.b66c31ab.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/static/js/2.b66c31ab.chunk.js.LICENSE"
+  },
+  {
+    "revision": "90a60445b057c314189b",
+    "url": "/static/js/main.879a218b.chunk.js"
+  },
+  {
+    "revision": "7a73c2f8e6fa773b68f4",
+    "url": "/static/js/runtime-main.74b87b24.js"
+  }
+]);
